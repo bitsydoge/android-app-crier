9404a0a2eeb985e22b58950168620ac20c204877
Cold <coldragon78@gmail.com>
2021-10-13 18:56:32 +0200

    Fix ImageHolder initialisation

--- app/src/main/java/com/cold0/crier/social/model/ImageHolder.kt
+++ app/src/main/java/com/cold0/crier/social/model/ImageHolder.kt
@@ -4,8 +4,8 @@ import androidx.compose.ui.graphics.Color
 import kotlin.random.Random
 
 data class ImageHolder(
-    var width: Int,
-    var height: Int,
+    var width: Int = 0,
+    var height: Int = 0,
     var colorAverage: Color = Color.White,
     var online: String = "https://i.pravatar.cc/300?img=${Random.nextInt(0, 71)}",
     var local: String? = null
