8032c0f2d4d482adcd2c0ed3cbdcf145e4b1a335
Cold <coldragon78@gmail.com>
2021-10-16 17:04:49 +0200

    Added Splashscreen using Android12 new API

--- app/src/main/java/com/cold0/crier/social/MainActivity.kt
+++ app/src/main/java/com/cold0/crier/social/MainActivity.kt
@@ -3,13 +3,14 @@ package com.cold0.crier.social
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.PaddingValues
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.cold0.crier.social.screens.MainScreen
 import com.cold0.crier.social.theme.CrierSocialTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             CrierSocialTheme {
                 MainScreen()
